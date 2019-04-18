8f6afd44da54d191bcb6827d36fd4166853a0ff8
andersonr <anderson.rissardi@hotmail.com>
2019-04-18 19:47:34 -0300

    fiquei doidão

--- config/auth/firebase_info_real.js
+++ config/auth/firebase_info_real.js
@@ -1,8 +1,18 @@
 export const firebaseConfig = {
-  apiKey: "******************************",
-  authDomain: "**************.firebaseapp.com",
-  databaseURL: "https://*************.firebaseio.com",
-  projectId: "***************",
-  storageBucket: "",
-  messagingSenderId: "***************"
+  apiKey: "AIzaSyD4wuFEhJ34tqrs-xo9--Vg6BTXqWLadbc",
+  authDomain: "bolaogot-276f8.firebaseapp.com",
+  databaseURL: "https://bolaogot-276f8.firebaseio.com",
+  projectId: "bolaogot-276f8",
+  storageBucket: "bolaogot-276f8.appspot.com",
+  messagingSenderId: "923130831542"
 };
+
+
+// var config = {
+      //   apiKey: "AIzaSyD4wuFEhJ34tqrs-xo9--Vg6BTXqWLadbc",
+      //   authDomain: "bolaogot-276f8.firebaseapp.com",
+      //   databaseURL: "https://bolaogot-276f8.firebaseio.com",
+      //   projectId: "bolaogot-276f8",
+      //   storageBucket: "bolaogot-276f8.appspot.com",
+      //   messagingSenderId: "923130831542"
+      // };
